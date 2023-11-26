75b1973483557cd2aeba28ce27b0f80084e79abc
ritesh <19ritkrs@gmail.com>
2023-11-25 21:32:00 -0800

    Your commit message

---
+++ src/main/java/com/demo/RestController.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/api")
+public class RestController {
+	
+	@GetMapping("/details")
+	public String getDetails() {
+		
+		return "Working fines";
+	}
+
+}
